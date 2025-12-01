037ab1acc68b5b3b29503d0d9a4be910047555aa
Muadh <muadh308@gmail.com>
2025-12-01 13:41:50 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
-    unoptimized: false,
+    unoptimized: true,
   },
 }
 
